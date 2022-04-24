53df9741f6a658e9676a84608e412e8771b6ff4d
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-04-24 17:59:23 -0500

    Stickers Fixed

--- src/handlers/stickerHandler.ts
+++ src/handlers/stickerHandler.ts
@@ -2,7 +2,30 @@ import { proto } from "@adiwajshing/baileys";
 import { imageStickerGenerator } from "../lib/imageStickerGenerator";
 import { videoStickerGenerator } from "../lib/videoStickerGenerator";
 
-// export async function stickerHandler(m: proto.IWebMessageInfo, messageType: string, sendStickerMessage: (contactId: string, stickerPath: string) => void, sender: string | null | undefined, sendTextMessage: (contactId: string, message: string) => void) {
+export async function stickerHandler(
+  m: proto.IWebMessageInfo,
+  messageType: string,
+  sendStickerMessage: (contactId: string, stickerPath: string) => void,
+  sender: string | null | undefined,
+  sendTextMessage: (contactId: string, message: string) => void
+) {
+  if (m.message!.imageMessage?.caption === "!stick") {
+    if (messageType === "imageMessage") {
+      // download stream
+      console.log("[STICKER]");
+      await imageStickerGenerator(m, sender!, sendStickerMessage);
+    }
 
-//   }
-// }
+    if (m.message!.videoMessage?.caption === "!stick") {
+      if (messageType === "videoMessage") {
+        console.log("[STICKER - ANIMATED]");
+        await videoStickerGenerator(
+          m,
+          sendStickerMessage,
+          sender,
+          sendTextMessage
+        );
+      }
+    }
+  }
+}
--- src/mainHandler.ts
+++ src/mainHandler.ts
@@ -1,4 +1,33 @@
-import { AnyMessageContent, AppStateChunk, AuthenticationCreds, BaileysEventEmitter, BaileysEventMap, BinaryNode, CatalogCollection, ChatModification, ConnectionState, Contact, GroupMetadata, MediaConnInfo, MessageReceiptType, MessageRelayOptions, MiscMessageGenerationOptions, OrderDetails, ParticipantAction, Product, ProductCreate, ProductUpdate, proto, SignalKeyStoreWithTransaction, WABusinessProfile, WAMediaUpload, WAMediaUploadFunction, WAPatchCreate, WAPatchName, WAPresence } from "@adiwajshing/baileys";
+import {
+  AnyMessageContent,
+  AppStateChunk,
+  AuthenticationCreds,
+  BaileysEventEmitter,
+  BaileysEventMap,
+  BinaryNode,
+  CatalogCollection,
+  ChatModification,
+  ConnectionState,
+  Contact,
+  GroupMetadata,
+  MediaConnInfo,
+  MessageReceiptType,
+  MessageRelayOptions,
+  MiscMessageGenerationOptions,
+  OrderDetails,
+  ParticipantAction,
+  Product,
+  ProductCreate,
+  ProductUpdate,
+  proto,
+  SignalKeyStoreWithTransaction,
+  WABusinessProfile,
+  WAMediaUpload,
+  WAMediaUploadFunction,
+  WAPatchCreate,
+  WAPatchName,
+  WAPresence,
+} from "@adiwajshing/baileys";
 import { randomNumber as randomNumberHandler } from "./handlers/randomGeneratorHandler";
 import { ytDownloadHandler } from "./handlers/ytDownloadHandler";
 import { stickerHandler } from "./handlers/stickerHandler";
@@ -10,14 +39,179 @@ import { videoStickerGenerator } from "./lib/videoStickerGenerator";
 export async function mainHandler(
   messages: proto.IWebMessageInfo[],
   sock: {
-    (contactId: string, buttons: proto.IButton[], caption: string, footer: string, imagePath: string): void; (arg0: string, arg1: { buttonId: string; buttonText: any; type: number; }[], arg2: string, arg3: string): void; getOrderDetails?: (orderId: string, tokenBase64: string) => Promise<OrderDetails>; getCatalog?: (jid?: string | undefined, limit?: number | undefined) => Promise<{ products: Product[]; }>; getCollections?: (jid?: string | undefined, limit?: number | undefined) => Promise<{ collections: CatalogCollection[]; }>; productCreate?: (create: ProductCreate) => Promise<Product>; productDelete?: (productIds: string[]) => Promise<{ deleted: number; }>; productUpdate?: (productId: string, update: ProductUpdate) => Promise<Product>; processMessage?: (msg: proto.IWebMessageInfo) => Promise<Partial<BaileysEventMap<any>>>; sendMessageAck?: ({ tag, attrs }: BinaryNode, extraAttrs: { [key: string]: string; }) => Promise<void>; sendRetryRequest?: (node: BinaryNode) => Promise<void>; appPatch?: (patchCreate: WAPatchCreate) => Promise<void>; sendPresenceUpdate?: (type: WAPresence, toJid?: string | undefined) => Promise<void>; presenceSubscribe?: (toJid: string) => Promise<void>; profilePictureUrl?: (jid: string, type?: "image" | "preview" | undefined, timeoutMs?: number | undefined) => Promise<string>; onWhatsApp?: (...jids: string[]) => Promise<{ exists: boolean; jid: string; }[]>; fetchBlocklist?: () => Promise<string[]>; fetchStatus?: (jid: string) => Promise<{ status: string; setAt: Date; }>; updateProfilePicture?: (jid: string, content: WAMediaUpload) => Promise<void>; updateBlockStatus?: (jid: string, action //Help Screen
-      : //Help Screen
-      "block" | "unblock") => Promise<void>; getBusinessProfile?: (jid: string) => Promise<void | WABusinessProfile>; resyncAppState?: (collections: WAPatchName[]) => Promise<AppStateChunk>; chatModify?: (mod: ChatModification, jid: string) => Promise<void>; resyncMainAppState?: () => Promise<void>; assertSessions?: (jids: string[], force: boolean) => Promise<boolean>; relayMessage?: (jid: string, message: proto.IMessage, { messageId: msgId, participant, additionalAttributes, cachedGroupMetadata }: MessageRelayOptions) => Promise<string>; sendReceipt?: (jid: string, participant: string, messageIds: string[], type: MessageReceiptType) => Promise<void>; sendReadReceipt?: (jid: string, participant: string, messageIds: string[]) => Promise<void>; readMessages?: (keys: proto.IMessageKey[]) => Promise<void>; refreshMediaConn?: (forceGet?: boolean | undefined) => Promise<MediaConnInfo>; waUploadToServer?: WAMediaUploadFunction; fetchPrivacySettings?: (force?: boolean | undefined) => Promise<{ [_: string]: string; }>; sendMessage?: (jid: string, content: AnyMessageContent, options?: MiscMessageGenerationOptions | undefined) => Promise<proto.WebMessageInfo>; groupMetadata?: (jid: string) => Promise<GroupMetadata>; groupCreate?: (subject: string, participants: string[]) => Promise<GroupMetadata>; groupLeave?: (id: string) => Promise<void>; groupUpdateSubject?: (jid: string, subject: string) => Promise<void>; groupParticipantsUpdate?: (jid: string, participants: string[], action: ParticipantAction) => Promise<string[]>; groupUpdateDescription?: (jid: string, description?: string | undefined) => Promise<void>; groupInviteCode?: (jid: string) => Promise<string>; groupRevokeInvite?: (jid: string) => Promise<string>; groupAcceptInvite?: (code: string) => Promise<string>; groupAcceptInviteV4?: (jid: string, inviteMessage: proto.IGroupInviteMessage) => Promise<string>; groupToggleEphemeral?: (jid: string, ephemeralExpiration: number) => Promise<void>; groupSettingUpdate?: (jid: string, setting: "announcement" | "locked" | "not_announcement" | "unlocked") => Promise<void>; groupFetchAllParticipating?: () => Promise<{ [_: string]: GroupMetadata; }>; type?: "md"; ws?: any; ev?: BaileysEventEmitter; authState?: { creds: AuthenticationCreds; keys: SignalKeyStoreWithTransaction; }; user?: Contact; emitEventsFromMap?: (map: Partial<BaileysEventMap<AuthenticationCreds>>) => void; assertingPreKeys?: (range: number, execute: (keys: { [_: number]: any; }) => Promise<void>) => Promise<void>; generateMessageTag?: () => string; query?: (node: BinaryNode, timeoutMs?: number | undefined) => Promise<BinaryNode>; waitForMessage?: (msgId: string, timeoutMs?: number | undefined) => Promise<any>; waitForSocketOpen?: () => Promise<void>; sendRawMessage?: (data: Buffer | Uint8Array) => Promise<void>; sendNode?: (node: BinaryNode) => Promise<void>; logout?: () => Promise<void>; end?: (error: Error) => void; onUnexpectedError?: (error: Error, msg: string) => void; uploadPreKeys?: (count?: number | undefined) => Promise<void>; waitForConnectionUpdate?: (check: (u: Partial<ConnectionState>) => boolean, timeoutMs?: number | undefined) => Promise<void>;
+    (
+      contactId: string,
+      buttons: proto.IButton[],
+      caption: string,
+      footer: string,
+      imagePath: string
+    ): void;
+    (
+      arg0: string,
+      arg1: { buttonId: string; buttonText: any; type: number }[],
+      arg2: string,
+      arg3: string
+    ): void;
+    getOrderDetails?: (
+      orderId: string,
+      tokenBase64: string
+    ) => Promise<OrderDetails>;
+    getCatalog?: (
+      jid?: string | undefined,
+      limit?: number | undefined
+    ) => Promise<{ products: Product[] }>;
+    getCollections?: (
+      jid?: string | undefined,
+      limit?: number | undefined
+    ) => Promise<{ collections: CatalogCollection[] }>;
+    productCreate?: (create: ProductCreate) => Promise<Product>;
+    productDelete?: (productIds: string[]) => Promise<{ deleted: number }>;
+    productUpdate?: (
+      productId: string,
+      update: ProductUpdate
+    ) => Promise<Product>;
+    processMessage?: (
+      msg: proto.IWebMessageInfo
+    ) => Promise<Partial<BaileysEventMap<any>>>;
+    sendMessageAck?: (
+      { tag, attrs }: BinaryNode,
+      extraAttrs: { [key: string]: string }
+    ) => Promise<void>;
+    sendRetryRequest?: (node: BinaryNode) => Promise<void>;
+    appPatch?: (patchCreate: WAPatchCreate) => Promise<void>;
+    sendPresenceUpdate?: (
+      type: WAPresence,
+      toJid?: string | undefined
+    ) => Promise<void>;
+    presenceSubscribe?: (toJid: string) => Promise<void>;
+    profilePictureUrl?: (
+      jid: string,
+      type?: "image" | "preview" | undefined,
+      timeoutMs?: number | undefined
+    ) => Promise<string>;
+    onWhatsApp?: (
+      ...jids: string[]
+    ) => Promise<{ exists: boolean; jid: string }[]>;
+    fetchBlocklist?: () => Promise<string[]>;
+    fetchStatus?: (jid: string) => Promise<{ status: string; setAt: Date }>;
+    updateProfilePicture?: (
+      jid: string,
+      content: WAMediaUpload
+    ) => Promise<void>;
+    updateBlockStatus?: (
+      jid: string,
+      action: //Help Screen
+      //Help Screen
+      "block" | "unblock"
+    ) => Promise<void>;
+    getBusinessProfile?: (jid: string) => Promise<void | WABusinessProfile>;
+    resyncAppState?: (collections: WAPatchName[]) => Promise<AppStateChunk>;
+    chatModify?: (mod: ChatModification, jid: string) => Promise<void>;
+    resyncMainAppState?: () => Promise<void>;
+    assertSessions?: (jids: string[], force: boolean) => Promise<boolean>;
+    relayMessage?: (
+      jid: string,
+      message: proto.IMessage,
+      {
+        messageId: msgId,
+        participant,
+        additionalAttributes,
+        cachedGroupMetadata,
+      }: MessageRelayOptions
+    ) => Promise<string>;
+    sendReceipt?: (
+      jid: string,
+      participant: string,
+      messageIds: string[],
+      type: MessageReceiptType
+    ) => Promise<void>;
+    sendReadReceipt?: (
+      jid: string,
+      participant: string,
+      messageIds: string[]
+    ) => Promise<void>;
+    readMessages?: (keys: proto.IMessageKey[]) => Promise<void>;
+    refreshMediaConn?: (
+      forceGet?: boolean | undefined
+    ) => Promise<MediaConnInfo>;
+    waUploadToServer?: WAMediaUploadFunction;
+    fetchPrivacySettings?: (
+      force?: boolean | undefined
+    ) => Promise<{ [_: string]: string }>;
+    sendMessage?: (
+      jid: string,
+      content: AnyMessageContent,
+      options?: MiscMessageGenerationOptions | undefined
+    ) => Promise<proto.WebMessageInfo>;
+    groupMetadata?: (jid: string) => Promise<GroupMetadata>;
+    groupCreate?: (
+      subject: string,
+      participants: string[]
+    ) => Promise<GroupMetadata>;
+    groupLeave?: (id: string) => Promise<void>;
+    groupUpdateSubject?: (jid: string, subject: string) => Promise<void>;
+    groupParticipantsUpdate?: (
+      jid: string,
+      participants: string[],
+      action: ParticipantAction
+    ) => Promise<string[]>;
+    groupUpdateDescription?: (
+      jid: string,
+      description?: string | undefined
+    ) => Promise<void>;
+    groupInviteCode?: (jid: string) => Promise<string>;
+    groupRevokeInvite?: (jid: string) => Promise<string>;
+    groupAcceptInvite?: (code: string) => Promise<string>;
+    groupAcceptInviteV4?: (
+      jid: string,
+      inviteMessage: proto.IGroupInviteMessage
+    ) => Promise<string>;
+    groupToggleEphemeral?: (
+      jid: string,
+      ephemeralExpiration: number
+    ) => Promise<void>;
+    groupSettingUpdate?: (
+      jid: string,
+      setting: "announcement" | "locked" | "not_announcement" | "unlocked"
+    ) => Promise<void>;
+    groupFetchAllParticipating?: () => Promise<{ [_: string]: GroupMetadata }>;
+    type?: "md";
+    ws?: any;
+    ev?: BaileysEventEmitter;
+    authState?: {
+      creds: AuthenticationCreds;
+      keys: SignalKeyStoreWithTransaction;
+    };
+    user?: Contact;
+    emitEventsFromMap?: (
+      map: Partial<BaileysEventMap<AuthenticationCreds>>
+    ) => void;
+    assertingPreKeys?: (
+      range: number,
+      execute: (keys: { [_: number]: any }) => Promise<void>
+    ) => Promise<void>;
+    generateMessageTag?: () => string;
+    query?: (
+      node: BinaryNode,
+      timeoutMs?: number | undefined
+    ) => Promise<BinaryNode>;
+    waitForMessage?: (
+      msgId: string,
+      timeoutMs?: number | undefined
+    ) => Promise<any>;
+    waitForSocketOpen?: () => Promise<void>;
+    sendRawMessage?: (data: Buffer | Uint8Array) => Promise<void>;
+    sendNode?: (node: BinaryNode) => Promise<void>;
+    logout?: () => Promise<void>;
+    end?: (error: Error) => void;
+    onUnexpectedError?: (error: Error, msg: string) => void;
+    uploadPreKeys?: (count?: number | undefined) => Promise<void>;
+    waitForConnectionUpdate?: (
+      check: (u: Partial<ConnectionState>) => boolean,
+      timeoutMs?: number | undefined
+    ) => Promise<void>;
   },
 
-
-
-  
   sendTextMessage: (contactId: string, message: string) => void,
   sendVideoMessage: (
     contactId: string,
@@ -43,23 +237,23 @@ export async function mainHandler(
   if (!m.message) return;
   if (m.key.fromMe) return;
   const chatId = m.key.remoteJid;
-  const senderId = m.key.participant
+  const senderId = m.key.participant;
   const messageType = Object.keys(m.message)[0];
   const splitMessage = m.message?.conversation?.split(" ") || "";
-  const splitMessageForBooks = m.message?.conversation?.split(",") || ""
+  const splitMessageForBooks = m.message?.conversation?.split(",") || "";
 
   const splitExtendedMessage =
-  m.message?.extendedTextMessage?.text?.split(" ") || "";
-  const message = m.message?.conversation
+    m.message?.extendedTextMessage?.text?.split(" ") || "";
+  const message = m.message?.conversation;
 
-  if (splitMessageForBooks[0] === "!book"){
-    console.log(splitMessageForBooks)
-    libGenHandler(splitMessageForBooks[1], chatId || "" ,sock)
+  if (splitMessageForBooks[0] === "!book") {
+    console.log(splitMessageForBooks);
+    libGenHandler(splitMessageForBooks[1], chatId || "", sock);
   }
 
   //Help Screen
   if (m.message?.conversation === "!comandos") {
-    console.log("[Baileys] Help Screen")
+    console.log("[Baileys] Help Screen");
     sendTextMessage(
       chatId!,
       "╭──┈ ➤ ✎ 【﻿ＭＥＮＵ】\n" +
@@ -79,7 +273,6 @@ export async function mainHandler(
     );
   }
 
-
   randomNumberHandler(m, sock);
   ytDownloadHandler(
     splitMessage,
@@ -90,29 +283,18 @@ export async function mainHandler(
     sendVideoMessage,
     sendAudioMessage
   );
-  // stickerHandler(
-  //   m,
-  //   messageType,
-  //   sendStickerMessage,
-  //   chatId,
-  //   sendTextMessage
-  // );
+  stickerHandler(
+    m,
+    messageType,
+    sendStickerMessage,
+    chatId,
+    sendTextMessage
+  );
 
-    if (m.message!.videoMessage?.caption === "!stick") {
-    if (messageType === "imageMessage") {
-      // download stream
-      console.log("[STICKER]")
-      await imageStickerGenerator(m, chatId! ,sendStickerMessage);
-  }
 
-    if (messageType === "videoMessage") {
-    console.log("[STICKER - ANIMATED]")
-      await videoStickerGenerator(m, sendStickerMessage, chatId, sendTextMessage);
+    if (splitExtendedMessage[0] === "!ban") {
+      console.log("[BAN]");
+      banHandler(sock, chatId, senderId, m, messages, sendTextMessage);
     }
-
-  if (splitExtendedMessage[0] === "!ban") {
-    console.log("[BAN]")
-    banHandler(sock, chatId, senderId, m, messages, sendTextMessage);
   }
-}
-}
+
