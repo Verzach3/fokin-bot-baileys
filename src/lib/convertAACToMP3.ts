1fd070087dba91237da42c1b40464ade6d560198
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-04-24 18:55:31 -0500

    Tune FFMpeg

--- src/lib/convertAACToMP3.ts
+++ src/lib/convertAACToMP3.ts
@@ -5,6 +5,6 @@ export async function convertAACToMp3(path: string, expath: string) {
     ff(path)
       .on("error", reject)
       .on("end", () => resolve(true))
-      .addOutputOptions([`-acodec libmp3lame`]).toFormat('mp3').save(expath);
+      .addOutputOptions([`-vn -b:a 192K -acodec libmp3lame`]).toFormat('mp3').save(expath);
   });
 }
