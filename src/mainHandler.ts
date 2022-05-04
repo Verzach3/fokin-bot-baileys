b25b0f1c05c14e507c5e055dc9843553622b5031
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-05-04 04:50:04 -0500

    Warn plz

--- src/mainHandler.ts
+++ src/mainHandler.ts
@@ -116,8 +116,7 @@ export async function mainHandler(
       await db.del(m.key.remoteJid!+m.message.extendedTextMessage!.contextInfo!.mentionedJid+"_warns");
       // sendTextMessage(m.message.extendedTextMessage?.contextInfo?.mentionedJid![0]!, "Ha sido expulsado del grupo");
       sendTextMessage(chatId!, "Mas de 3 warns, expulsado del grupo");
-      // sock.groupParticipantsUpdate!(m.key.remoteJid!, [...m.message.extendedTextMessage?.contextInfo?.mentionedJid!], "remove");
-      sock.groupParticipantsUpdate!(messages[0].key.remoteJid!, [...m.message!.extendedTextMessage?.contextInfo?.mentionedJid!], "remove");
+      banHandler(sock, chatId, senderId, m, messages, sendTextMessage);
       return;
     }
     else{
